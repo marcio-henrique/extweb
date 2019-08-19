8c41e2511a4af15a4a4cbdeb1c36d3ba5f2e880e
michelthomas <michel.lins17@gmail.com>
2019-08-18 23:55:49 -0300

    MongoDB initial config;

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const bodyParser = require('body-parser');
 
 const fs = require('fs');
 
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, (err) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log("Connected!");
+});
+
+
 // logger
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
