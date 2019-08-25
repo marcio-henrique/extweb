b7ac124d8fc6521de72106815520fb0b763e5a3f
Hellena Canuto <52940272+trojan-exe@users.noreply.github.com>
2019-08-25 08:23:33 -0300

    update na tela de detalhes

--- public/js/detalhes_acao.js
+++ public/js/detalhes_acao.js
@@ -14,14 +14,56 @@ fetch('/projetos')
                 data.forEach(projeto => {
 
                     const { titulo, tipo_curso, modalidade, resumo, area_principal,
-                        tipo_acao, local, espaco_realizacao, periodo_inscricao, vagas,
-                        programacao, publico_alvo, carga_horaria, link_inscricao, equipe,
+                        tipo_acao, local, espaco_realizacao, periodo_inscricao, vagas, programacao, 
+                        publico_alvo, carga_horaria, link_inscricao, equipe,
                         imagens} = projeto;
-
+                    
+                    
                     document.getElementById("titulo").innerText = titulo;
-
                     document.getElementById("txtDescricao").innerText = resumo;
 
+                    //especificações
+                    document.getElementById("publicoAlvo").innerText = publico_alvo;
+                    document.getElementById("modalidade").innerText = modalidade;
+                    document.getElementById("areaPrincipal").innerText = area_principal;
+                    document.getElementById("tipoAcao").innerText = tipo_acao;
+                    document.getElementById("cargaHoraria").innerText = carga_horaria;
+
+                    //inscrições
+                    document.getElementById("inicioInscricoes").innerText = periodo_inscricao.data_inicial;
+                    document.getElementById("fimInscricoes").innerText = periodo_inscricao.data_final;
+                    document.getElementById("linkInscricoes").href = link_inscricao;
+
+
+                    //programação
+                    document.getElementById("txtDataP").innerText = programacao[0].data;
+                    document.getElementById("inicioP").innerText = programacao[0].horario_inicial;
+                    document.getElementById("fimP").innerText = programacao[0].horario_final;
+                    document.getElementById("atividadesP").innerText = programacao[0].atividades;
+
+                    //local
+                    document.getElementById("cidade").innerText = local.municipio;
+                    document.getElementById("estado").innerText = local.estado;
+                    document.getElementById("espaco").innerText = espaco_realizacao;
+
+                    //vagas                
+                    document.getElementById("vagasdisponiveis").innerText = vagas.quantidade_total;
+                    document.getElementById("vagasocupadas").innerText = vagas.quantidade_ocupada;
+                    
+                    //equipe
+                    document.getElementById("nomeContato0").innerText = equipe[0].nome;
+                    document.getElementById("categoriaContato0").innerText = equipe[0].categoria;
+                    document.getElementById("funcaoContato0").innerText = equipe[0].funcao;
+                    document.getElementById("emailContato0").innerText = equipe[0].email;
+                    document.getElementById("nomeContato1").innerText = equipe[1].nome;
+                    document.getElementById("categoriaContato1").innerText = equipe[1].categoria;
+                    document.getElementById("funcaoContato1").innerText = equipe[1].funcao;
+                    document.getElementById("emailContato1").innerText = equipe[1].email;
+                    document.getElementById("nomeContato2").innerText = equipe[2].nome;
+                    document.getElementById("categoriaContato2").innerText = equipe[2].categoria;
+                    document.getElementById("funcaoContato2").innerText = equipe[2].funcao;
+                    document.getElementById("emailContato2").innerText = equipe[2].email;
+                
                     //set images on carousel
                     var carousel = document.getElementById("carrossel");
                     var slide_list = document.getElementById("slideList");
@@ -51,12 +93,7 @@ fetch('/projetos')
                         carousel.appendChild(carr);
                         slide_list.appendChild(slide);
 
-                    });
-
-
-                    publico_alvo.forEach(publico => {
-                        console.log(publico);
-                    });
+                    }); 
 
                     // const b = document.createElement('b');
                     // const p1 = document.createElement('p');
@@ -75,6 +112,5 @@ fetch('/projetos')
 
                     // document.getElementById("tipo_curso").innerText = tipo_curso;
                     // document.getElementById("modalidade").innerText = modalidade;
-                    // document.getElementById("resumo").innerText = resumo;
                 });
             }
